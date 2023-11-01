c9a0ab038775137f8e168ebe47b91492a85e4ea4
KwanTNg <128802089+KwanTNg@users.noreply.github.com>
2023-11-01 09:13:03 +0000

    1st commit

---
+++ Tic-Tac-Toe/app.js
@@ -0,0 +1,72 @@
+const gameBoard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+const startCells = ["", "", "", "", "", "", "", "", ""];
+
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
+function createBoard() {
+  startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
+    gameBoard.append(cellElement);
+  });
+}
+
+createBoard();
+
+function addGo(e) {
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  e.target.append(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "it is now " + go + "'s go.";
+  e.target.removeEventListener("click", addGo);
+  checkScore();
+}
+// e.target.append add a nested element inside the square
+// removeEventListener makes the same squre cannot be clicked again
+// remember to call chekcScore function after a new cross/circle is added, to check to see if the winning codition meet
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  console.log(allSquares);
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("cross")
+    );
+    if (crossWins) {
+      infoDisplay.textContent = "Cross Wins!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+}
